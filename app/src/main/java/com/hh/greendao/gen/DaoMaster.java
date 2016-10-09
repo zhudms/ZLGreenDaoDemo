d947447558c9f832d92739de22b98a8242aa56f6
TangXingmeng <zhudms@163.com>
2016-10-09 18:24:10 +0800

    keepTrying
    keepTrying

--- app/src/main/java/com/hh/greendao/gen/DaoMaster.java
+++ app/src/main/java/com/hh/greendao/gen/DaoMaster.java
@@ -21,12 +21,12 @@ public class DaoMaster extends AbstractDaoMaster {
 
     /** Creates underlying database table using DAOs. */
     public static void createAllTables(Database db, boolean ifNotExists) {
-        DBRootBeanDao.createTable(db, ifNotExists);
+        MedicalInspectBDao.createTable(db, ifNotExists);
     }
 
     /** Drops underlying database table using DAOs. */
     public static void dropAllTables(Database db, boolean ifExists) {
-        DBRootBeanDao.dropTable(db, ifExists);
+        MedicalInspectBDao.dropTable(db, ifExists);
     }
 
     /**
@@ -45,7 +45,7 @@ public class DaoMaster extends AbstractDaoMaster {
 
     public DaoMaster(Database db) {
         super(db, SCHEMA_VERSION);
-        registerDaoClass(DBRootBeanDao.class);
+        registerDaoClass(MedicalInspectBDao.class);
     }
 
     public DaoSession newSession() {
--- app/src/main/java/com/zl/zxylgreendaodemo/bean/DBRootBean.java
+++ app/src/main/java/com/zl/zxylgreendaodemo/bean/DBRootBean.java
@@ -18,40 +18,9 @@ public class DBRootBean<T> {
     public String parentTag;
     @Generated
     public int pageNo;
-    @Generated(hash = 554560037)
-    public DBRootBean(int singleKey, String insertTime, String parentTag, int pageNo) {
-        this.singleKey = singleKey;
-        this.insertTime = insertTime;
-        this.parentTag = parentTag;
-        this.pageNo = pageNo;
-    }
-    @Generated(hash = 633868061)
-    public DBRootBean() {
-    }
-    public int getSingleKey() {
-        return this.singleKey;
-    }
-    public void setSingleKey(int singleKey) {
-        this.singleKey = singleKey;
-    }
-    public String getInsertTime() {
-        return this.insertTime;
-    }
-    public void setInsertTime(String insertTime) {
-        this.insertTime = insertTime;
-    }
-    public String getParentTag() {
-        return this.parentTag;
-    }
-    public void setParentTag(String parentTag) {
-        this.parentTag = parentTag;
-    }
-    public int getPageNo() {
-        return this.pageNo;
-    }
-    public void setPageNo(int pageNo) {
-        this.pageNo = pageNo;
-    }
-
+    @Generated
+    public MedicalInspectB  bean;
+    @Generated
+    public MedicalInspectDetailB beanDetail;
 
 }
